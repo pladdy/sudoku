69c647aceafad597866fd77d7288b4023914a4eb
Matt Pladna <pladdypants@gmail.com>
2020-10-25 23:29:51 -0400

    Add grid methods; create solver script

--- sudoku/grid.py
+++ sudoku/grid.py
@@ -1,3 +1,41 @@
+MAX_ARRAY_COUNT = MAX_ROWS = 9
+MAX_ARRAY_SUM = 45
+COMPLETE_ARRAY = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
 class Grid:
     def __init__(self):
-        self.foo = "bar"
+        self.grid_type = "grid"
+        self.rows = []
+        self.columns = [[], [], [], [], [], [], [], [], []]
+
+    def add_row(self, numbers):
+        self.validate_array(numbers)
+        if len(self.rows) >= MAX_ROWS:
+            raise RuntimeError("Grid is full: no more rows can be added")
+
+        self.columns.append([])
+        for i, val in enumerate(numbers):
+            self.columns[i].append(val)
+
+        self.rows.append(numbers)
+
+    def show(self):
+        for row in self.rows:
+            print(row)
+
+    def validate_array(self, numbers):
+        if type(numbers).__name__ != "list":
+            raise TypeError("Invalid argument, must be a list")
+        if len(numbers) != MAX_ARRAY_COUNT:
+            raise ValueError("Invalid array: too many numbers")
+        if len(set(numbers)) != len(numbers):
+            raise ValueError("Invalid row: row has duplicate numbers")
+        if sum(numbers) > MAX_ARRAY_SUM:
+            raise ValueError("Invalid row: row total is too high")
+
+
+# TODO: inheritance vs composition; when a grid is made, create subgrids?
+class SubGrid(Grid):
+    def __init__(self):
+        self.grid_type = "subgrid"
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,6 +1,59 @@
+import io
+import pytest
+import sys
+
 import sudoku.grid as grid
 
 
-def test_grid():
-    g = grid.Grid()
-    assert g.foo == "bar"
+class TestGrid:
+    def test_grid(self):
+        g = grid.Grid()
+        assert g.grid_type == "grid"
+
+    def test_grid_add_row_exceptions(self):
+        g = grid.Grid()
+
+        tests = [None, "foo", 1, {"a": 4}, {3, 4, 5}, (3, 4, 5)]
+        for test in tests:
+            with pytest.raises(TypeError):
+                g.add_row(test)
+
+        tests = [[1], [1, 2, 3, 4, 5, 6, 7, 8, 8], [1, 2, 3, 4, 5, 6, 7, 10, 8]]
+        for test in tests:
+            with pytest.raises(ValueError):
+                g.add_row(test)
+
+        with pytest.raises(RuntimeError):
+            # modify the row count internally to quickly verify you can't
+            # add more than 9 rows
+            g.rows = [[], [], [], [], [], [], [], [], []]
+            g.add_row([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_grid_show(self):
+        _sysout = sys.stdout
+        sys.stdout = result = io.StringIO()
+
+        g = grid.Grid()
+        g.add_row([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        g.show()
+
+        sys.stdout = _sysout
+        assert result.getvalue() == "[1, 2, 3, 4, 5, 6, 7, 8, 9]\n"
+
+    def test_grid_validate_array(self):
+        g = grid.Grid()
+
+        tests = [None, "foo", 1, {"a": 4}, {3, 4, 5}, (3, 4, 5)]
+        for test in tests:
+            with pytest.raises(TypeError):
+                g.validate_array(test)
+
+        tests = [[1], [1, 2, 3, 4, 5, 6, 7, 8, 8], [1, 2, 3, 4, 5, 6, 7, 10, 8]]
+        for test in tests:
+            with pytest.raises(ValueError):
+                g.validate_array(test)
+
+
+def test_subgrid():
+    g = grid.SubGrid()
+    assert g.grid_type == "subgrid"
